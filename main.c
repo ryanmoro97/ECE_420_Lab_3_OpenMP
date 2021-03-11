7c9b90f5b6014c9abb87f98ddcd759639166eca3
Ryan <moro@ualberta.ca>
2021-03-11 02:54:33 -0700

    use guided to speedup elimination

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int elimination(int thread_count){
     double *x;
     int i, k, j, row, max, max_index, local_max, local_index;
     int *index;
-    // local_max=0;
-    // local_index=0;
     double start, end, temp;
 
     if(Lab3LoadInput(&A, &size) == 1){
@@ -32,26 +30,28 @@ int elimination(int thread_count){
 
 
     // eliminate elements below the diagonal to zero one column after another
-    for(k = 0; k < size-1; k++){
-        /*Pivoting
-        from row k to row n, find the row kp that has the maximum
-        absolute value of the element in the kth column */
+#pragma omp parallel num_threads(thread_count) default(none) shared(A, x, size, index, max, max_index, thread_count) private(temp, k, row, i, j, local_max, local_index)
+    {
+        for (k = 0; k < size - 1; k++) {
 
-        // Parallelize inner loop instead of outer since outer has dependencies. You could maybe try the outer loop with a lot more private stuff
-        #pragma omp parallel num_threads(thread_count) private(row, i, j, local_max, local_index)
-        {
+            /*Pivoting
+            from row k to row n, find the row kp that has the maximum
+            absolute value of the element in the kth column */
+
+            // Parallelize inner loop instead of outer since outer has dependencies. You could maybe try the outer loop with a lot more private stuff
+
             max = 0;
-            local_max=0;
-            local_index=0;
-            #pragma omp parallel for num_threads(thread_count)
-            for(row = k; row < size; row++){
-                if(fabs(A[index[row]][k]) > local_max){
+            local_max = 0;
+            local_index = 0;
+            #pragma omp for //parallel for num_threads(thread_count)
+            for (row = k; row < size; row++) {
+                if (fabs(A[index[row]][k]) > local_max) {
                     local_max = fabs(A[index[row]][k]);
                     local_index = row;
                 }
             }
-            if(local_max > max){
-                #pragma omp critical
+            if (local_max > max) {
+            #pragma omp critical
                 max = local_max;
                 max_index = local_index;
             }
@@ -61,53 +61,55 @@ int elimination(int thread_count){
             #pragma omp single
             {
                 if (max_index != k) {
-                    row = index[max_index];
+                    row = index[max_index];s
                     index[max_index] = index[k];
                     index[k] = row;
                 }
             }
             // Gaussian elimination
-            #pragma omp single
-            {for(i = k+1; i < size; i++){
+//            #pragma omp single
+//            {
+            #pragma omp for schedule(guided)
+            for (i = k + 1; i < size; i++) {
                 temp = A[index[i]][k] / A[index[k]][k];
                 // could maybe put a parallel for loop here with a critical section and barrier
-                for(j = k; j < size+1; j++){
+                for (j = k; j < size + 1; j++) {
                     A[index[i]][j] = A[index[i]][j] - temp * A[index[k]][j];
                 }
             }
-          }
+//            }
 
         }
-    }
+
 
         // Jordan elimination
-    // Could try the outer loop and make a lot more private variables
-    for(k = size-1; k > 0; k--){
-        #pragma omp parallel num_threads(thread_count)
-        {
-            #pragma omp parallel for num_threads(thread_count) shared(A) firstprivate(k) private(i)
-            for(i = 0; i < k; i++){
-              // ciritcal section and barrier here because for increasing values of k, some stuff might get overwritten if the order is wrong
-              #pragma omp critical
-                A[index[i]][size] = A[index[i]][size] - ((A[index[i]][k] / A[index[k]][k]) * A[index[k]][size]);
-                A[index[i]][k] = 0;
-            }
-            #pragma omp barrier
+        // Could try the outer loop and make a lot more private variables
+        for (k = size - 1; k > 0; k--) {
+//            #pragma omp parallel num_threads(thread_count)
+//            {
+                // scheduled here because for increasing values of k, some stuff might get overwritten if the order is wrong
+                #pragma omp for schedule(guided)
+                for (i = 0; i < k; i++) {
+                    A[index[i]][size] = A[index[i]][size] - ((A[index[i]][k] / A[index[k]][k]) * A[index[k]][size]);
+                    A[index[i]][k] = 0;
+                }
+//            }
         }
-    }
 
 
-    // Final solution
-    #pragma omp parallel num_threads(thread_count)
-    {
-      #pragma omp parallel for num_threads(thread_count)
-      for(i = 0; i < size; i++){
-          x[i] = A[index[i]][size] / A[index[i]][i];
-      }
+        // Final solution
+//        #pragma omp parallel num_threads(thread_count)
+//        {
+            #pragma omp for //parallel for num_threads(thread_count)
+            for (i = 0; i < size; i++) {
+                x[i] = A[index[i]][size] / A[index[i]][i];
+            }
+//        }
     }
 
     GET_TIME(end);
     Lab3SaveOutput(x, size, end-start);
+    printf("%f\n", end-start);
     DestroyVec(x);
     DestroyMat(A, size);
     free(index);
@@ -120,5 +122,8 @@ int main(int argc, char* argv[]){
         exit(1);
     }
     int thread_count = atoi(argv[1]);
+
     elimination(thread_count);
+//
+    return 0;
 }
